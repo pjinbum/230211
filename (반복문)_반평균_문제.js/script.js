9ecc6ec28ef874b64708b0d5fff938730c8bc443
pjinbum <onepjb@naver.com>
2023-02-11 18:09:05 +0900

    ⚡️ compleat

--- (반복문)_반평균_문제.js/script.js
+++ (반복문)_반평균_문제.js/script.js
@@ -2,15 +2,17 @@
 
 function answer(score) {
   let average = 0;
-  
+   sum = 0;
+   average = 0;
+
   for (let i = 0; i < score.length; i++) {
-    average += score[i];
+    sum += score[i];
+    // sum = sum + score[i]
   }
-  
-  average = average / score.length;
-  
-  return average.toFixed(2);
-  // let average = 0;
+
+  average = sum / score.length;
+  // return average.toFixed(2);
+  return average;
   // // 코드 구현 
   
   // for (let i = 0; i < score.length; i++) {
@@ -31,9 +33,7 @@ let input = [
   [82, 77, 51, 64, 73, 90, 80],
   [100, 71, 59, 88, 72, 75, 91, 93],
 ];
-// min = Math.min.apply(null, input)
-input =Math.min(null , input );
-console.log(input);
+
 
 for (let i = 0; i < input.length; i++) {
   console.log(`#${i + 1} ${answer(input[i])}`);
