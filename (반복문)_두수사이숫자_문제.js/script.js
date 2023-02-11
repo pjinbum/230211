602ebb46cfbe0a209e8193c88cc94199fe28be64
pjinbum <onepjb@naver.com>
2023-02-11 14:43:06 +0900

    ⚡️ 문제

---
+++ (반복문)_두수사이숫자_문제.js/script.js
@@ -0,0 +1,21 @@
+function answer(x, y) {
+  let result = [];
+  // 코드 구현 
+  return result;
+}
+
+
+let input = [
+    [3, 7],
+    [8, 3],
+    [12, 10],
+];
+
+for (let i = 0; i < input.length; i++) {
+  console.log(answer(input[i][0], input[i][1]));
+}
+
+// 결과
+// #1 [ 3, 4, 5, 6, 7 ]
+// #2 [ 3, 4, 5, 6, 7, 8 ]
+// #3 [ 10, 11, 12 ]
---
+++ (조건,반복)_최솟값구하기_문제.js/script.js
@@ -0,0 +1,22 @@
+
+function answer(x, y) {
+  let min;
+  // 코드 구현
+  return min;
+}
+
+
+let input = [
+  [16, 3],
+  [-3, 1],
+  [1000, 525],
+];
+
+for (let i = 0; i < input.length; i++) {
+  console.log(`#${i + 1} ${answer(input[i][0], input[i][1])}`);
+}
+
+// 결과
+// #1 3
+// #2 -3
+// #3 525
